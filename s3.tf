6e96b7c6d7121ca65ee8da130620a0d310715a9e
sora <blue.solar1999@gmail.com>
2025-01-13 16:41:26 +0900

    ✨ ログバケットの作成

--- s3.tf
+++ s3.tf
@@ -71,3 +71,39 @@ resource "aws_s3_bucket_cors_configuration" "public" {
     max_age_seconds = 3000
   }
 }
+
+# ログバケット
+resource "aws_s3_bucket" "alb_log" {
+  bucket = "s-aoki-alb-log-pragmatic-terraform"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+
+  rule {
+    id     = "log_expiration"
+    status = "Enabled"
+
+    expiration {
+      days = 180
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+  policy = data.aws_iam_policy_document.alb_log.json
+}
+
+data "aws_iam_policy_document" "alb_log" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [var.aws_account_id]
+    }
+  }
+}
--- varialbles.tf
+++ varialbles.tf
@@ -2,3 +2,8 @@ variable "aws_profile" {
   description = "AWS CLI Profile"
   type        = string
 }
+
+variable "aws_account_id" {
+  description = "AWS Account ID"
+  type        = string
+}
