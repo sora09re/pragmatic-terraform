94432901677f56f80fb86db014a80af1c3893664
sora <blue.solar1999@gmail.com>
2025-01-19 10:39:33 +0900

    ✨ ログの設定、ECSタスク実行ロールの作成

--- ecs.tf
+++ ecs.tf
@@ -9,6 +9,11 @@ resource "aws_ecs_task_definition" "example" {
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   container_definitions    = file("./container_definitions.json")
+  execution_role_arn       = module.ecs_task_execution_role.iam_role_arn
+
+  tags = {
+    Name = "s-aoki-example"
+  }
 }
 
 resource "aws_ecs_service" "example" {
@@ -47,4 +52,30 @@ module "nginx_sg" {
   vpc_id      = aws_vpc.example.id
   port        = 80
   cidr_blocks = [aws_vpc.example.cidr_block]
+}
+
+resource "aws_cloudwatch_log_group" "for_ecs" {
+  name              = "/ecs/s-aoki-example"
+  retention_in_days = 7
+}
+
+data "aws_iam_policy" "ecs_task_execution_role_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+data "aws_iam_policy_document" "ecs_task_execution" {
+  source_policy_documents = [data.aws_iam_policy.ecs_task_execution_role_policy.policy]
+
+  statement {
+    effect    = "Allow"
+    actions   = ["ssm:GetParameters", "kms:Decrypt"]
+    resources = ["*"]
+  }
+}
+
+module "ecs_task_execution_role" {
+  source     = "./iam_role"
+  name       = "s-aoki-ecs-task-execution"
+  identifier = "ecs-tasks.amazonaws.com"
+  policy     = data.aws_iam_policy_document.ecs_task_execution.json
 }
